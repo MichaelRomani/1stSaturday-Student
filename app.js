1acef3690abec32b982d77e0d52b7804a6c6527d
Mike Romani <mr4227@gmail.com>
2018-02-08 14:46:30 -0500

    feat/saturday help tickets

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(morgan('dev'));
+
+app.use('/student', Student);
+app.use('/test', Test);
+
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.listen(3000, function() {
+  console.log('Server is listening on port 3000!');
+})
---
+++ readme.md
@@ -0,0 +1,2 @@
+npm i express morgan body-parser --save
+npm i nodemon -g

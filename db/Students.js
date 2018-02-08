3c2689304472a868d7a56c629ff9c2201b670496
Mike Romani <mr4227@gmail.com>
2018-02-08 16:33:49 -0500

    feat: routes - model - studens

--- db/Students.js
+++ db/Students.js
@@ -1,38 +1,37 @@
-let number = 3;
-
-let Students = {
+const Students = {
+  idIncrementor: 3,
   students: [
     {
       id: 1,
-      firsName: 'Ada',
-      lastName: 'Lovelace',
-      enrolled: true
+      firstName: 'Ada',
+      lastName: 'Lovelace'
     },
     {
       id: 2,
-      firsName: 'Grace',
-      lastName: 'Hopper',
-      enrolled: true
+      firstName: 'Grace',
+      lastName: 'Hopper'
     },
     {
       id: 3,
-      firsName: 'Jean',
-      lastName: 'Bartik',
-      enrolled: true
+      firstName: 'Jean',
+      lastName: 'Bartik'
     }
   ],
   findAll: function () {
     return this.students;
   },
   create: function (student) {
-    student.id = ++number;
+    student.id = ++this.idIncrementor;
     this.students.push(student);
   },
   destroy: function (id) {
     this.students = this.students.filter(student => student.id !== +id);
   },
   findById: function (id) {
-    return this.students.filter(student => student.id === +id)
+    return this.students.find(student => student.id === +id);
+  },
+  update: function (studentUpdate, id) {
+    this.students[+id - 1] = Object.assign({}, this.students[+id - 1], studentUpdate);
   }
 };
 
--- routes/students.js
+++ routes/students.js
@@ -6,22 +6,27 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/:studentId', function(req, res, next) {
-  res.json(Students.findById(req.params.studentId))
+  if(Students.findById(req.params.studentId) === undefined) {
+    res.sendStatus(404);
+  }
+  else {
+    res.json(Students.findById(req.params.studentId));
+  }
 });
 
 router.post('/', function(req, res, next) {
-  Students.create(req.body)
-  res.json(Students.findAll())
+  Students.create(req.body);
+  res.status(201).json(Students.findAll());
 });
 
 router.put('/:id', function(req, res, next) {
-
+  Students.update(req.body, req.params.id);
+  res.status(203).json(Students.findAll());
 });
 
 router.delete('/:id', function(req, res, next) {
-  Students.destroy(req.params.id)
-  const allStudents = Students.findAll()
-  res.json(allStudents)
+  Students.destroy(req.params.id);
+  res.json(Students.findAll());
 });
 
  module.exports = router;

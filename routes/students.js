6bb432ef8e5443dd650647871528c1c92cc5d739
Mike Romani <mr4227@gmail.com>
2018-02-13 15:02:54 -0500

    starting place

--- routes/students.js
+++ routes/students.js
@@ -2,36 +2,12 @@ const router = require('express').Router();
 const Students = require('../db/students');
 
 //Get all students
-router.get('/', function(req, res, next) {
-  console.log('in here')
-  res.json(Students.findAll())
-});
 
 //Get student by id
-router.get('/:studentId', function(req, res, next) {
-  if (Students.findById(req.params.studentId) === undefined) {
-    res.sendStatus(404);
-  }
-  else {
-    res.json(Students.findById(req.params.studentId));
-  }
-});
 
 //Create a new student and send status of 201
-router.post('/', function(req, res, next) {
-  res.status(201).json(Students.create(req.body));
-});
 
 //Update a student
-router.put('/:id', function(req, res, next) {
-  if (!Students.destroy(req.params.id)) return res.sendStatus(404);
-  res.status(203).json(Students.update(req.body, req.params.id));
-});
 
 //Delete a student
-router.delete('/:id', function(req, res, next) {
-  if (!Students.destroy(req.params.id)) return res.sendStatus(404);
-  res.json(Students.destroy(req.params.id));
-});
 
- module.exports = router;
--- routes/tests.js
+++ routes/tests.js
@@ -2,36 +2,15 @@ const router = require('express').Router();
 const Test = require('../db/Tests');
 
 //Get all tests
-router.get('/', function(req, res, next) {
-  res.json(Test.findAll());
-});
 
 //Get all tests with scores at or above 70
-router.get('/passing', function(req, res, next) {
-  res.json(Test.passing());
-});
+
 
 //Get test by id
-router.get('/:id', function(req, res, next) {
-  if (!Test.findById(req.params.id)) return res.sendStatus(404);
-  res.json(Test.findById(req.params.id));
-});
 
 //Get all tests of a particular subject
-router.get('/subject/:subject', function(req, res, next) {
-  if (!Test.findBySubject(req.params.subject)) return res.sendStatus(404);
-  res.json(Test.findBySubject(req.params.subject));
-});
 
 //Create a new test and send status of 201
-router.post('/', function(req, res, next) {
-  res.status(201).json(Test.create(req.body));
-});
 
 //Delete a test
-router.delete('/:id', function(req, res, next) {
-  if (!Test.destroy(req.params.id)) return res.sendStatus(404);
-  res.json(Test.destroy(req.params.id));
-});
 
-module.exports = router;
